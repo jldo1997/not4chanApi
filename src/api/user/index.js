b800ec0aa489068105e29f632045adc7b29ff2ad
unknown <josdomoli@gmail.com>
2019-03-14 09:20:36 +0100

    solucion errores menores

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { middleware as query } from 'querymen'
 import { middleware as body } from 'bodymen'
 import { password as passwordAuth, master, token } from '../../services/passport'
-import { index, showMe, show, create, update, updatePassword, destroy } from './controller'
+import { index, showMe, show, create, update, updatePassword, destroy, banHammer } from './controller'
 import { schema } from './model'
 export User, { schema } from './model'
 
