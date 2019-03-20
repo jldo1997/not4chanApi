c5664ab0bc587ca2ad25ec16a29526a85730cd88
unknown <josdomoli@gmail.com>
2019-03-20 22:15:00 +0100

    aun mas

--- src/api/comment/controller.js
+++ src/api/comment/controller.js
@@ -10,14 +10,15 @@ export const create = ({ user, bodymen: { body } }, res, next) =>
     .then(success(res, 201))
     .catch(next)
 
-export const createAlexCorasonsito = ({ user, bodymen: { body }, photo }, res, next) =>{
-  uploadService.uploadFromBinary(body.file.buffer)
+export const createAlexCorasonsito = (req, res, next) =>{
+  uploadService.uploadFromBinary(req.file.buffer)
       .then(json => Photo.create({
             url: json.data.link,
             deletehash: json.data.deletehash
           }))
           .then((photo) => {
-            Comment.create({ ...body, user, photo})
+            body = req.body;
+            Comment.create({ body , photo})
             .then((comment) => comment.view(true))
           })
           .then(success(res, 201))
